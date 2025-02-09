c6632485cd8de1e12bcac79c6eee65dc1bdaf0f5
Samuel-Labagnere <samuel.labagnere@gmail.com>
2025-02-09 13:44:10 +0100

    Fixed articles list

--- html/scripts/article/list.sql
+++ html/scripts/article/list.sql
@@ -1,4 +1,4 @@
 SELECT articles.*, authors.name AS author_name, COUNT(comments.id) AS comments FROM articles
 JOIN authors ON author_id = authors.id
-JOIN comments ON articles.id = comments.article_id
-GROUP BY articles.id;
+LEFT JOIN comments ON articles.id = comments.article_id
+GROUP BY articles.id, authors.name;
