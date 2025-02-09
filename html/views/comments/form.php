736d3f79e64c225f26f865952769343c8a078454
Samuel-Labagnere <samuel.labagnere@gmail.com>
2025-02-09 13:48:52 +0100

    Switching title whether adding or updating

--- html/views/comments/form.php
+++ html/views/comments/form.php
@@ -8,14 +8,16 @@ check_connexion();
 if (!empty($_GET["comment"])) {
   $comment = view_comment($_GET["comment"]);
   $action = "/actions/comment/update.php";
-} elseif (!empty($_GET["article"])) {
+  $title = "Modifier votre commentaire";
+} else {
   $article = view_article($_GET["article"]);
   $action = "/actions/comment/add.php";
+  $title = "Ajouter un commentaire";
 }
 ?>
 
 <form action=<?= $action ?> method="POST" class="form">
-  <h3>Ajouter un commentaire</h3>
+  <h3><?= $title ?></h3>
   <textarea name="content" required><?= $comment['content'] ?? '' ?></textarea>
   <? if (isset($comment)): ?>
     <input name="id" type="number" class="hidden" value="<?= $comment['id'] ?>" />
